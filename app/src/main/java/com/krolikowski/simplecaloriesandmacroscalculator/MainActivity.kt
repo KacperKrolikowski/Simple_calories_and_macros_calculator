84828f25cd12b2707a4a8f2130d2c6261f28e52e
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2020-11-24 01:36:56 +0100

    Początki

--- app/src/main/java/com/krolikowski/simplecaloriesandmacroscalculator/MainActivity.kt
+++ app/src/main/java/com/krolikowski/simplecaloriesandmacroscalculator/MainActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var weight: Int = 0
+    var height: Int = 0
+    var age = 0
+    var sex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun countCalories (wei: Int, hei: Int, ag: Int, se: Int) = (10*wei)+(6.25*hei)-(5*ag) + se
 }
