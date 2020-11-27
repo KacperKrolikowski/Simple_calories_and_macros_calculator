6885a1fc42c8b226dff2823c8c2fb57fe45c5d4e
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2020-11-27 14:28:32 +0100

    SplashScreen almost complete

--- app/src/main/java/com/krolikowski/simplecaloriesandmacroscalculator/SplashScreenActivity.kt
+++ app/src/main/java/com/krolikowski/simplecaloriesandmacroscalculator/SplashScreenActivity.kt
@@ -1,13 +1,33 @@
 package com.krolikowski.simplecaloriesandmacroscalculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_splash_screen)
 
 
+        supportActionBar!!.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+
+        val splashScreenDuration = getSplashScreenDuration()
+        Handler().postDelayed(
+            {
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+            },
+            splashScreenDuration
+        )
+
     }
+
+    private fun getSplashScreenDuration() = 5000L
 }
+
